7b74a2acab5dbd485c0c43f57505bbe07af67fdb
RhexNiebres <rhexniebres@gmail.com>
2025-02-07 20:33:46 -0800

    Change main route "root" with App

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,18 +1,16 @@
 import App from './App'
 import ErrorPage from './Pages/ErrorPage.jsx'
-import Root from './Pages/Root.jsx';
 import Shop from './Pages/ShopPage.jsx';
 import About from './Pages/AboutPage.jsx';
 import Cart from './Pages/CartPage.jsx';
 import ItemCards from './Components/ItemCards.jsx'
 const routes = [
     {path:'/' ,
-        element:<Root/>,
+        element:<App/>,
         errorElement:<ErrorPage/>,
         children:[
             {index: true, element: 
             <>
-            <App/>
             <About/>
             </>},
             {
