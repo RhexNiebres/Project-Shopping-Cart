38ebdeb0f8bd32199208b54cc394dd2ab6ee10bc
RhexNiebres <rhexniebres@gmail.com>
2025-02-08 21:23:35 -0800

    Style: Add style for image and text element

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
-import ErrorImage from "../assets/ErrorImage.jpg"
+import ErrorImage from "../assets/ErrorImage.jpg";
 
 const ErrorPage = () => {
   return (
     <div>
-      <img src= {ErrorImage} alt="Error Image" />
-      <h1>Oh no, this route doesn&apos;t exist!</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+      <img className="mx-auto h-96 w-96" src={ErrorImage} alt="Error Image" />
+      <h1 className="text-center">Oh no, this route doesn&apos;t exist!</h1>
+      <div className="text-center">
+        <Link to="/">
+          You can go back to the home page by clicking{" "}
+          <button className="w-15 h-9 bg-black p-2 text-orange-500">
+            {" "}
+            here{" "}
+          </button>
+          though!
+        </Link>
+      </div>
     </div>
   );
 };
