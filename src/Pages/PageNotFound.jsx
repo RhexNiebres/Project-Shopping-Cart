afbfd51505b99b80874511d10942ca27a06ecd7c
RhexNiebres <rhexniebres@gmail.com>
2025-08-11 13:34:20 +0800

    style: add styling on PageNotFound

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="text-center bg-orange-400 p-10 rounded text-white font-bold text-2xl">
-        <h1 className="text-center p-4">Oh no, this route does not exist!</h1>
+    <div className="flex flex-col justify-center items-center h-screen bg-black">
+      <div className="text-center bg-orange-500 p-10 rounded text-white font-bold text-2xl">
+        <h1 className="text-center p-4">Oh no, this page does not exist!</h1>
         <Link to="/">
           You can go back to the home page by clicking{" "}
           <button className="w-15  bg-black px-4 py-2 text-white hover:text-orange-500 rounded">
-            {" "}
-            here{" "}
+            
+            here.
           </button>
         </Link>
       </div>
